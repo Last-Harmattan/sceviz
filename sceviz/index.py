181e9cbdb0ede82cf189636cb06f5393b960c124
linarion <paul.scholz@uni-rostock.de>
2021-03-17 13:26:02 +0100

    parse_schema now bundles semantic information in the nodes instead of creating them as seperate nodes

--- sceviz/index.py
+++ sceviz/index.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#Reads JSON-Schema and converts it to Cytoscape.js format
+# Reads JSON-Schema and converts it to Cytoscape.js format
 
-from flask import (
-    Blueprint, render_template, url_for, current_app
-)
+from flask import (Blueprint, render_template, url_for, current_app)
 
 import json
 
-bp = Blueprint('index',__name__)
+bp = Blueprint('index', __name__)
+
 
 @bp.route('/')
 def index():
     schema = parse_schema(load_schema('uploads/schema.json'))
     return render_template('base.html', schema=schema)
 
+
 def load_schema(path: str) -> dict:
     """Loads JSON-Schema from file and returns a dict
-        
+
         Parameters
         ----------
         path : str
             Path to JSON-Schema file
-        
+
         Returns
         -------
         dict
@@ -32,16 +32,18 @@ def load_schema(path: str) -> dict:
         schema = json.load(file)
         return schema
 
-def create_node(id: str, label: str = None) -> dict:
+
+def create_node(id: str, label: str = None, node_type: str = None,
+                description: str = None, content: str = None) -> dict:
     """Creates a Cytoscape node
-        
+
         Parameters
         ----------
         id : str
-            Unique identifier in the form of an absolute path to that node     
+            Unique identifier in the form of an absolute path to that node
         label : str, optional
             A name for displaying
-            
+
         Returns
         -------
         dict
@@ -52,11 +54,14 @@ def create_node(id: str, label: str = None) -> dict:
         Cytoscape format:
             { data: { id: 1, label: 'some label' } }
     """
-    return { 'data': { 'id': id, 'label': label }, 'grabbable': False }
+    return {'data': {'id': id, 'label': label, 'type': node_type,
+            'description': description, 'content': content},
+            'grabbable': False}
+
 
 def create_edge(id: str, source: str, target: str) -> dict:
     """Creates a Cytoscape edge
-        
+
         Parameters
         ----------
         id : str
@@ -78,14 +83,15 @@ def create_edge(id: str, source: str, target: str) -> dict:
             { data: { id: 'A->B', source: 'A', target: 'B'  } }
     """
 
-    return { 'data': { 'id': id, 'source': source, 'target': target } } 
+    return {'data': {'id': id, 'source': source, 'target': target}}
 
-    
 
-def parse_schema(schema: dict, parent: str = "#",
-                data: list = [], index: str = "") -> list:
+def parse_schema(schema: dict,
+                 parent: str = "#",
+                 data: list = [],
+                 index: str = "") -> list:
     """Takes JSON-Schema and converts it to cytoscape format
-        
+
         Parameters
         ----------
         schema : dict
@@ -103,36 +109,52 @@ def parse_schema(schema: dict, parent: str = "#",
             A list dictionaries conforming to the Cytoscape format.
     """
     if not data:
-        #Data is empty i.e. current node is root
-        try:
-            #Use specific title if given
-            data.append(create_node('#',schema['title']))
-        except KeyError:
-            #and generic if not
-            data.append(create_node('#','root'))
+        # Data is empty i.e. current node is root
+        data.append(create_node('#',schema.get('title', 'root'),
+                                    schema.get('type', None),
+                                    schema.get('description', None)))
 
     for key, value in schema.items():
-        #Referencing requires unique id for nodes, we use the path from root
+        # Referencing requires unique id for nodes, we use the path from root
         id = parent + '/' + key + index
         edge_id = parent + '->' + key + index
-        
+
         if isinstance(value, dict):
-            #If the child node is a dict we can call parse_schema() recursivly
-            data.append(create_node(id,key))
-            data.append(create_edge(edge_id,parent,id))
-            parse_schema(value,id,data)
+            # If the child node is a dict we can call parse_schema() recursivly
+
+            # Check for annotations
+            node_type = value.get('type', 'JSON Schema')
+            description = value.get('description', None)
+            node_label = value.get('title', key)
+
+            if not isinstance(node_label, str):
+                node_label = key
+            
+            data.append(create_node(id, node_label, node_type, description))
+            data.append(create_edge(edge_id, parent, id))
+            parse_schema(value, id, data)
         elif isinstance(value, list):
-            #If the child node is a list we have to handle that differently
-            #depending on the list's items' type
-            data.append(create_node(id,key))
-            data.append(create_edge(edge_id,parent,id))
-            #Arrays can lead to redundant ids and thus we number them to ensure
-            #uniqueness.
+            # If the child node is a list we have to handle that differently
+            # depending on the list's items' type
+            content = None
+
+            if key == 'type':
+                continue
+
+            if any( isinstance(elem,str) for elem in value ):
+                content = value
+
+            data.append(create_node(id, key, 'array', content=content))
+            data.append(create_edge(edge_id, parent, id))
+            # Arrays can lead to redundant ids and thus we number them to
+            # ensure uniqueness.
             for elem in enumerate(value):
                 if isinstance(elem[1], dict):
-                    parse_schema(elem[1],id,data,str(elem[0]))
+                    parse_schema(elem[1], id, data, str(elem[0]))
+        elif key in ['title','type','description']:
+            continue
         else:
-            data.append(create_node(id,key))
-            data.append(create_edge(edge_id,parent,id))
+            data.append(create_node(id, key, content=value))
+            data.append(create_edge(edge_id, parent, id))
 
     return data
